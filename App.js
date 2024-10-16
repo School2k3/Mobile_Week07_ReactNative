811eb4ff57b49557769a7e98ffd2fadf66c49e3a
School2k3 <truong29042003@gmail.com>
2024-10-16 17:37:02 +0700

    done step 02: add, edit, delete (re-render)

--- App.js
+++ App.js
@@ -10,96 +10,81 @@ import {
   Image,
 } from 'react-native';
 
-const data = [
-  {
-    id: 1,
-    content: 'To check email',
-  },
-  {
-    id: 2,
-    content: 'UI task web page',
-  },
-  {
-    id: 3,
-    content: 'Learn javascript basic',
-  },
-  {
-    id: 4,
-    content: 'Learn HTML Advance',
-  },
-  {
-    id: 5,
-    content: 'Medical App UI',
-  },
-  {
-    id: 6,
-    content: 'Learn Java',
-  },
-];
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-// useEffect(() => {
-//   fetch('https://645464baa74f994b333f63ab.mockapi.io/account')
-//   .then(response => response.json)
-// }, []);
+const ListOfContents = () => {
+  const [data, setData] = useState([]);
 
-const Item = ({ content }) => (
+  useEffect(() => {
+    var fn = fetch('https://645464baa74f994b333f63ab.mockapi.io/account');
+    fn.then((response) => response.json()).then((newdata) => setData(newdata));
+  });
+
+  var addContent = function () {
+    fetch('https://645464baa74f994b333f63ab.mockapi.io/account', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        content: 'Learn mobile programming',
+      }),
+    })
+    .then(res => res.json());
+  };
+
+  var updateContent = function (id) {
+    fetch(`https://645464baa74f994b333f63ab.mockapi.io/account/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        content: 'Learn Java',
+      }),
+    })
+    .then(res => res.json());
+  };
+
+  var deleteContent = function (id) {
+    fetch(`https://645464baa74f994b333f63ab.mockapi.io/account/${id}`, {
+      method: 'DELETE',
+    });
+  };
+
+  const Item = ({ content }) => (
   <View style={styles.item}>
-  <Image source={require('./icons/Frame.png')}/>
-    <Text style={styles.content}>{content}</Text>
-    <Image style={styles.editIcon} source={require('./icons/Edit.png')}/>
+    <TouchableOpacity onPress={() => deleteContent(content.id)}><Image source={require('./icons/Frame.png')}/></TouchableOpacity>
+    <Text style={styles.content}>{content.content}</Text>
+    <TouchableOpacity onPress={() => updateContent(content.id)}><Image style={styles.editIcon} source={require('./icons/Edit.png')}/></TouchableOpacity>
   </View>
 );
 
-const App = () => {
   return (
     <View style={styles.container}>
-      <View style={{ flex: 2 }}></View>
+      <View style={{ flex: 1 }}></View>
       <SafeAreaView style={{ flex: 2 }}>
         <FlatList
           data={data}
-          renderItem={({ item }) => <Item content={item.content} />}
+          renderItem={({ item }) => <Item content={item} />}
           keyExtractor={(item) => item.id}
         />
       </SafeAreaView>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => {
-          fetch('https://645464baa74f994b333f63ab.mockapi.io/account', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-              id: 7,
-              content: 'Learn mobile programming',
-            }),
-          });
-        }}>
-        <Text>Create</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => {
-          fetch('https://645464baa74f994b333f63ab.mockapi.io/account/7', {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-              content: 'Learn English',
-            }),
-          });
-        }}>
-        <Text>Update</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => {
-          fetch('https://645464baa74f994b333f63ab.mockapi.io/account/7', {
-            method: 'DELETE'
-          });
-        }}>
-        <Text>Delete</Text>
+      <TouchableOpacity style={styles.addButton} onPress={() => addContent()}>
+        <Image source={require('./icons/addButton.png')}/>
       </TouchableOpacity>
     </View>
   );
 };
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='' component={ListOfContents} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -107,11 +92,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#ecf0f1',
     padding: 8,
-  },
-  button: {
-    backgroundColor: 'cyan',
-    paddingVertical: 12,
-    marginBottom: 4,
   },
   item: {
     flexDirection: 'row',
@@ -123,16 +103,23 @@ const styles = StyleSheet.create({
     marginBottom: 8,
     marginLeft: 12,
     marginRight: 12,
-    borderRadius: 24
+    borderRadius: 24,
   },
   content: {
     flex: 2,
     fontWeight: 'bold',
-    marginLeft: 12
+    marginLeft: 12,
   },
   editIcon: {
-    marginRight: 16
+    marginRight: 16,
+  },
+  addButton: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
 
 export default App;
+
+
